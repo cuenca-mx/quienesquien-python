84225ef8056a6891799550be98681571661ea213
gabino <gabino@cuenca.com>
2025-02-06 17:32:33 -0600

    Update project dependencies and configuration

--- quienesquien/client.py
+++ quienesquien/client.py
@@ -1,57 +1,67 @@
+from dataclasses import dataclass
+from typing import TypedDict
+
 import requests
 
 from .person import PERSON_FIELDNAMES, Person
 
 
+class SearchResult(TypedDict):
+    resumen: dict[str, int | bool]
+    persons: list[Person]
+
+
+@dataclass
 class Client:
+    client_url: str
+    client_user: str
+    client_id: str
+    secret_id: str
+    percent: int = 80
 
-    client_user = None
-    client_url = None
-    client_id = None
-    secret_id = None
-    percent = 80
-
-    def __init__(self, url: str, user: str, client_id: str, secret_id: str):
-        """
-        Configura el endpoint y las credenciales para realizar busquedas
-        :param url: https://peps.mx/datos/qws
-        :param user: usuario
-        :param client_id: client_id
-        :param secret_id: secret_id
-        :return:
-        """
-        self.client_user = user
-        self.client_url = url
-        self.client_id = client_id
-        self.secret_id = secret_id
-
-    def search(self, nombre, paterno, materno):
+    def _get_token(self) -> str:
         login_url = f'{self.client_url}/api/token?client_id={self.client_id}'
         headers = {'Authorization': f'Bearer {self.secret_id}'}
 
         login_response = requests.get(login_url, headers=headers)
+        login_response.raise_for_status()
 
-        token = login_response.text
+        return login_response.text
 
-        url = f'{self.client_url}/api/find?client_id={self.client_id}'\
-        f'&username={self.client_user}&percent={self.percent}'\
-        f'&name={nombre} {paterno} {materno}'
+    def search(self, nombre: str, paterno: str, materno: str) -> SearchResult:
+        token = self._get_token()
+
+        url = (
+            f'{self.client_url}/api/find?client_id={self.client_id}'
+            f'&username={self.client_user}&percent={self.percent}'
+            f'&name={nombre} {paterno} {materno}'
+        )
 
         headers = {'Authorization': f'Bearer {token}'}
         response = requests.get(url, headers=headers)
+        response.raise_for_status()
 
         data = response.json()
 
         persons = []
-        for person in data.get('data', []):
-            kwargs = dict()
+
+        kwargs = dict()
+
+        persons_data = data.get('data', [])
+
+        for person in persons_data:
             for field in PERSON_FIELDNAMES:
-                if field in person:
-                    kwargs[field] = person[field]
+                if person.get(field.upper()) is not None:
+                    field_value = person.get(field.upper())
+                    kwargs[field] = field_value
             persons.append(Person(kwargs))
 
-        resumen = dict(
-            success=data['success'],
-            num_registros=len(persons),
-        )
-        return dict(resumen=resumen, persons=persons)
+        result: SearchResult = {
+            'resumen': {
+                'success': data['success'],
+                'num_registros': len(persons),
+            },
+            'persons': persons,
+        }
+
+        return result
