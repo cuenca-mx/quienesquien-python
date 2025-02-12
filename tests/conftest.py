56e48510779d653fb80dc1c9b759d1f20fbcb854
gabino <gabino@cuenca.com>
2025-02-11 21:54:44 -0600

    Simplify VCR configuration for token handling in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,24 +15,13 @@ def client() -> Generator[Client, None, None]:
     yield Client(QEQ_USER, QEQ_CLIENT_ID, QEQ_SECRET_ID)
 
 
-def mask_first_response(
-    response: dict[str, Any], interaction_count: int
-) -> dict[str, Any]:
-    """Mask the first response to avoid exposing the response token"""
-    if interaction_count == 0 and response['body']['string']:
-        response['body']['string'] = b'DUMMY'
-    return response
-
-
 @pytest.fixture(scope='module')
 def vcr_config() -> dict[str, Any]:
-    request_counter = 0
 
-    def before_record_response(response: dict[str, Any]) -> dict[str, Any]:
-        nonlocal request_counter
-        response = mask_first_response(response, request_counter)
-        request_counter += 1
-        return response
+    def filter_token(request):
+        if request.path == '/api/token':
+            return None
+        return request
 
     config: dict[str, Any] = dict()
     config['filter_headers'] = [
@@ -43,11 +32,5 @@ def vcr_config() -> dict[str, Any]:
         ('username', 'DUMMY_USERNAME'),
     ]
 
-    config['before_record_response'] = before_record_response
+    config['before_record_request'] = filter_token
     return config
-
-
-@pytest.fixture(autouse=True, scope='function')
-def reset_request_counter(vcr_config: dict[str, Any]) -> None:
-    """Reset the request counter in each test"""
-    vcr_config['before_record_response'].__closure__[0].cell_contents = 0
